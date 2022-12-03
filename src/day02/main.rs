a3afd8587cc7f2b31504eb6366ea5fef08c60255
Alexandre Brun <alexandre.brun@zendesk.com>
2022-12-03 14:23:06 -0500

    day02 & day03 part 1

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -1,22 +1,72 @@
 use std::fmt::Display;
 use std::time::Instant;
 
-use aoc_2022::get_input;
+use aoc_2022::get_input_as_string;
 
-fn solve(input: &[String]) -> (impl Display, impl Display) {
-    (0, 0)
+fn calculate_score(input: &str, version: &str) -> u32 {
+    input
+        .split("\n")
+        .filter(|a| !a.is_empty())
+        .map(|x| {
+            let ennemy: i32 = (x.chars().nth(0).unwrap() as i32) - 65;
+            let mut myself: i32 = (x.chars().nth(2).unwrap() as i32) - 88;
+
+            if "v2".eq(version) {
+                myself = (ennemy + myself + 2) % 3;
+            }
+
+            let hand_score = myself + 1;
+            let fight_score = ((myself - ennemy + 4) % 3) * 3;
+
+            hand_score + fight_score
+        })
+        .sum::<i32>() as u32
+}
+
+fn solve(input: &str) -> (impl Display, impl Display) {
+    let p1 = calculate_score(input, "v1");
+    let p2 = calculate_score(input, "v2");
+
+    (p1, p2)
 }
 
 fn main() {
-    let input = get_input("day02.txt");
+    let input = get_input_as_string("day02.txt");
 
     let start = Instant::now();
 
-    let (r1, r2) = solve(input.as_slice());
+    let (r1, r2) = solve(&input);
 
     let t = start.elapsed().as_nanos() as f64 / 1000.0;
 
     println!("Part 1: {}", r1);
     println!("Part 2: {}", r2);
     println!("Duration: {:.3}μs", t);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::calculate_score;
+
+    #[test]
+    fn test_p1() {
+        let input = "A Y
+B X
+C Z";
+
+        let res = calculate_score(&input, "v1");
+
+        assert_eq!(15, res);
+    }
+}
+
+#[test]
+fn test_p2() {
+    let input = "A Y
+B X
+C Z";
+
+    let res = calculate_score(&input, "v2");
+
+    assert_eq!(12, res);
 }
--- src/day03/main.rs
+++ src/day03/main.rs
@@ -1,22 +1,66 @@
+use std::collections::HashSet;
 use std::fmt::Display;
 use std::time::Instant;
 
-use aoc_2022::get_input;
+use aoc_2022::get_input_as_string;
 
-fn solve(input: &[String]) -> (impl Display, impl Display) {
-    (0, 0)
+fn get_total_priority(input: &str) -> u32 {
+    input
+        .split("\n")
+        .filter(|a| !a.is_empty())
+        .map(|x| {
+            let (left, right) = x.split_at(x.len() / 2);
+
+            let left_items: HashSet<char> = left.chars().collect();
+            let right_items: HashSet<char> = right.chars().collect();
+
+            let intersect: char = *left_items.intersection(&right_items).into_iter().next().unwrap();
+
+            if intersect.is_lowercase() {
+                (intersect as u32) - 96
+            } else {
+                (intersect as u32) - 38
+            }
+        })
+        .sum()
+}
+
+fn solve(input: &str) -> (impl Display, impl Display) {
+    let p1 = get_total_priority(input);
+    let p2 = 9;
+
+    (p1, p2)
 }
 
 fn main() {
-    let input = get_input("day02.txt");
+    let input = get_input_as_string("day03.txt");
 
     let start = Instant::now();
 
-    let (r1, r2) = solve(input.as_slice());
+    let (r1, r2) = solve(&input);
 
     let t = start.elapsed().as_nanos() as f64 / 1000.0;
 
     println!("Part 1: {}", r1);
     println!("Part 2: {}", r2);
     println!("Duration: {:.3}μs", t);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::get_total_priority;
+
+    #[test]
+    fn test_p1() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+        let res = get_total_priority(&input);
+
+        assert_eq!(157, res);
+    }
 }
