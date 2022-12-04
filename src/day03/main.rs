1d69d47e780458440938735dc531278c011c0df7
Alexandre Brun <alexandre.brun@zendesk.com>
2022-12-03 22:33:14 -0500

    day03 part2

--- src/day03/main.rs
+++ src/day03/main.rs
@@ -4,7 +4,15 @@ use std::time::Instant;
 
 use aoc_2022::get_input_as_string;
 
-fn get_total_priority(input: &str) -> u32 {
+fn calculate_priority(item: &char) -> u32 {
+    if item.is_lowercase() {
+        (*item as u32) - 96
+    } else {
+        (*item as u32) - 38
+    }
+}
+
+fn get_items_priority(input: &str) -> u32 {
     input
         .split("\n")
         .filter(|a| !a.is_empty())
@@ -15,19 +23,36 @@ fn get_total_priority(input: &str) -> u32 {
             let right_items: HashSet<char> = right.chars().collect();
 
             let intersect: char = *left_items.intersection(&right_items).into_iter().next().unwrap();
-
-            if intersect.is_lowercase() {
-                (intersect as u32) - 96
-            } else {
-                (intersect as u32) - 38
-            }
+            calculate_priority(&intersect)
         })
         .sum()
 }
 
+fn get_badges_priority(input: &str) -> u32 {
+    let mut sum: u32 = 0;
+
+    input
+        .split("\n")
+        .filter(|a| !a.is_empty())
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .for_each(|x| {
+            let first: HashSet<char> = x[0].chars().collect();
+            let second: HashSet<char> = x[1].chars().collect();
+            let third: HashSet<char> = x[2].chars().collect();
+
+            let first_and_second = first.intersection(&second).map(|c| *c).collect::<HashSet<char>>();
+            let intersect: char = *first_and_second.intersection(&third).into_iter().next().unwrap();
+
+            sum += calculate_priority(&intersect);
+        });
+
+    sum
+}
+
 fn solve(input: &str) -> (impl Display, impl Display) {
-    let p1 = get_total_priority(input);
-    let p2 = 9;
+    let p1 = get_items_priority(input);
+    let p2 = get_badges_priority(input);
 
     (p1, p2)
 }
@@ -48,7 +73,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::get_total_priority;
+    use crate::get_badges_priority;
+    use crate::get_items_priority;
 
     #[test]
     fn test_p1() {
@@ -59,8 +85,22 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw";
 
-        let res = get_total_priority(&input);
+        let res = get_items_priority(&input);
 
         assert_eq!(157, res);
+    }
+
+    #[test]
+    fn test_p2() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+        let res = get_badges_priority(&input);
+
+        assert_eq!(70, res);
     }
 }
